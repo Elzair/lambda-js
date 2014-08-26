139fd978b019179982502a27686c6781e4cdc6b0
Philip Woods <elzairthesorcerer@gmail.com>
2014-08-26 10:57:52 -0400

    Refactored env code into its own submodule and fixed additional errors

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
-var op = require('mathops');
+var env  = require(__dirname+'/lib/env')
+  , util = require('util')
+  ;
 
-var create_env = function(outer, keys, vals) { // An environment: a dict of {'var': value} pairs, with an outer env
-  var properties = {}; 
-  keys = keys || {}; vals = vals || {};
-  for (var i=0; i<keys.length; i++) {
-    properties[keys[i]] = vals[i];
-  }
-  return Object.create(outer || {}, {
-      properties: {value: properties}
-    , outer: {value: outer}
-    , find: {
-        value: function(prop) {
-          var self = this;
-          while (!self.properties.hasOwnProperty(prop) && self.outer !== null) {
-            self = self.outer;
-          }
-          return self.properties;
-        }
-      }
-  });
-};
+var global_env = env.add_globals(env.create_env());
 
-var add_globals = function(env) { // Add some Scheme standard procedures to an environment
-  Object.defineProperties(env || {}, {
-    properties: {
-      value: {
-        '+': op.add, '-': op.sub, '*': op.mul, '/': op.div, 'not': op.not,
-        '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
-        'equal?': op.eq, 'eq?': op.steq, 'length': function(x) {return x.length;}, 'cons': function (x, y) {return [x].concat(y);},
-        'car': function(x) {return x[0];}, 'cdr': function(x) {return x.slice(1);}, 'append': op.add,
-        'list': function(x) {return [x];}, 'list?': function(x) {return Array.isArray(x);},
-        'null?': function(x) {return x===[];}, 'symbol?': function(x) {return typeof x === 'string';}
-      }
-    }
-  });
-  return env;
-};
-
-var global_env = create_env(add_globals(null));
-
-var lisp_eval = function(x, env) {  // Evaluate an expression in an environment.
+var lisp_eval = exports.lisp_eval = function(x, env) {  // Evaluate an expression in an environment.
   env = env || global_env;
   var alt, conseq, exp, exps, test, v, val, vs;
 
-  if (typeof x === 'string') {      // variable reference
-    return env.find(x)[x];
+  if (typeof x === 'string' && isNaN(x)) {      // variable reference
+    console.log('Variable Reference: ' + x);
+    console.log(util.format('Env: %j', env.find(x)));
+    return env.find(x).properties[x];
   }
-  else if (!Array.isArray(x)) {     // constant literal
+  else if (typeof x === 'string' && !isNaN(x)) {     // constant literal
+    console.log('Constant Literal: ' + x);
     return x;
   }
   else if (x[0] === 'quote') {      // (quote exp)
+    console.log('(' + x + ')');
     exp = x[1];
     return exp;
   }
-  else if (x[0] === 'if') {         // (if test conseq alt)
+  else if (x[0] === 'if') {         // (if test conseq alt
+    console.log('(' + x + ')');
     test = x[1]; conseq = x[2]; alt = x[3];
     return lisp_eval([lisp_eval(test) ? conseq : alt], env);
   }
   else if (x[0] === 'set!') {       // (set! var exp)
+    console.log('(' + x + ')');
     v = x[1]; exp = x[2];
     env.find(v)[v] = lisp_eval(exp, env);
   }
   else if (x[0] === 'define') {     // (define var exp)
+    console.log('(' + x + ')');
     v = x[1]; exp = x[2];
     env.properties[v] = lisp_eval(exp, env);
   }
   else if (x[0] === 'lambda') {     // (lambda (var*) exp)
+    console.log('(' + x + ')');
     vs = x[1]; exp = x[2];
-    return function(args) { lisp_eval(exp, create_env(env, vs, args)); };
+    return function(args) { lisp_eval(exp, env.create_env(env, vs, args)); };
   }
   else if (x[0] === 'begin') {      // (begin expr*)
+    console.log('(' + x + ')');
     exps = x.splice(1);
     for (var ex in exps) {
       val = lisp_eval(ex, env);
@@ -77,8 +51,10 @@ var lisp_eval = function(x, env) {  // Evaluate an expression in an environment.
     return val;
   }
   else {                            // (proc exp*)
+    console.log('(proc ' + x + ')');
+    exps = [];
     for (var exx in x) {
-      exps = exps === null ? [lisp_eval(exx, env)] : exps.push(lisp_eval(exx, env));
+      exps.push(lisp_eval(x[exx], env));
     }
     return exps[0](exps.slice(1));
   }
@@ -89,7 +65,9 @@ var read = exports.parse = function(s) {  // Read a Scheme expression from a str
 };
 
 var tokenize = function(s) {              // Convert a string into a list of tokens
-  return s.replace('(', ' ( ').replace(')', ' ) ').split();
+  var tokens = s.replace('(', ' ( ').replace(')', ' ) ').split(/\s+/);
+  console.log(tokens);
+  return tokens.slice(1, tokens.length-1);
 };
 
 var read_from = function(tokens) {        // Read an expression from a sequence of tokens
@@ -98,6 +76,7 @@ var read_from = function(tokens) {        // Read an expression from a sequence 
     throw new Error("SyntaxError: Unexpected EOF while reading!");
   }
   var token = tokens.shift();
+  console.log(token);
   if (token === '(') {
     L = [];
     while (tokens[0] !== ')') {
@@ -121,7 +100,11 @@ var atom = function(token) {
 var repl = exports.repl = function(prompt) {
   process.stdout.write(prompt);
   process.stdin.on('data', function(data) {
-    var val = lisp_eval(read(data.toString()));
+    //var val = lisp_eval(read(data.toString()));
+    var tokens = read(data.toString());
+    console.log(tokens);
+    var val = lisp_eval(tokens, global_env);
+    console.log(val);
     process.stdout.write('\n\n');
     if (!val) {
       //process.stdout.write(to_s(val));
